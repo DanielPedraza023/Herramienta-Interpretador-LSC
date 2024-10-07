d9d86b036008d3497f3ed62b95f9567335f3159f
DanielPedraza509 <pedrazajaca2001@gmail.com>
2024-10-07 00:07:35 -0500

    Velocidad de audio disminuida

--- TextToAudio.py
+++ TextToAudio.py
@@ -2,8 +2,16 @@ import pyttsx3
 
 def reproducir_audio(palabra):
     engine = pyttsx3.init()
+    
+    # Configurar la velocidad (la velocidad predeterminada es alrededor de 200 WPM)
+    rate = engine.getProperty('rate')  # Obtener la velocidad actual
+    engine.setProperty('rate', rate - 40)  # Reducir la velocidad en 50 WPM (ajusta según prefieras)
+    
+    
     engine.say(palabra)
     engine.runAndWait()
+
+
 
 
 #detector = "HOLA"
